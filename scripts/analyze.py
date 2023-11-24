68d61cdfdc9d96a76b433840b916262f6a3552c8
davilage.borges@gmail.com <davilage.borges@gmail.com>
2023-11-24 10:50:16 -0300

    Change temperature

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -4,6 +4,7 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 
+
 def main():
     load_dotenv()
     rootpath = os.path.dirname(os.getcwd())
@@ -19,7 +20,8 @@ def main():
         model="gpt-3.5-turbo",
         messages=[{"role": "system", "content": 'Você é um atendente de farmácia capaz de extrair informações de receitas médicas e transformá-las em JSON. O formato do JSON é {medicamentos: [{nome, dosagem, posologia, duracao, ingredientes: [{nome, quantidade}]\}]}. O campo nome deve conter o nome do medicamento ou suplemento. O campo dosagem deve conter a dosagem e a unidade de medida. O campo posologia deve conter a quantidade a ser utilizada por dia (somente um número inteiro). O campo duração deve conter a duração em dias do tratamento ou 1 caso não seja possível calcular. O texto contém prescrições médicas não estruturadas em português obtidas por OCR. Observe que \'qsp\' significa \'quantidade suficiente para\', manipular X significa que X é a quantidade a ser fabricada do medicamento/suplemento.'
                    },
-                  {"role": "user", "content": f"{input_text}"}]
+                  {"role": "user", "content": f"{input_text}"}],
+        temperature=0.1
     )
 
     filename = sys.argv[1].split(".")[0]
