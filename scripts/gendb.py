2fa5fdde3dbce2649d8b4d25a8b736d4c14a7ffd
davilage.borges@gmail.com <davilage.borges@gmail.com>
2023-12-10 18:08:28 -0300

    Added new medicine to database

--- scripts/gendb.py
+++ scripts/gendb.py
@@ -1,14 +1,15 @@
 import sqlite3
-from random import randint
+from random import uniform
+from unidecode import unidecode
 
 con = sqlite3.connect("pharmacy.db")
 cur = con.cursor()
 cur.execute("CREATE TABLE medicines(name, dosage, value)")
-with open("medicines.txt", "r") as sourc:
+with open("Ativos.csv", "r") as sourc:
     for line in sourc.readlines():
-        line = line.split(":")
-        value = randint(1, 50)
-        print(f"INSERT INTO medicines VALUES ('{line[0].upper()}', '{line[1].lstrip().rstrip()}', {value});")
+        line = line.split(";")
+        value = round(uniform(1, 3), 2)
+        print(f"INSERT INTO medicines VALUES ('{unidecode(line[0].upper())}', '{line[1].lstrip().rstrip()}', {value});")
         cur.execute(f"INSERT INTO medicines VALUES ('{line[0].upper()}', '{line[1].lstrip().rstrip()}', {value});")
         cur.execute('COMMIT')
 
