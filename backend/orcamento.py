ffda484149311ba22dc40210f525be37662447dc
Yuri Pereira <yuri@anaai.com.br>
2024-11-26 15:58:53 -0300

    Creating getPrice to update orcamento value. Adequating some functions to calc price correctly

--- backend/orcamento.py
+++ backend/orcamento.py
@@ -1,9 +1,11 @@
 import pandas as pd, re
+import utils
 
 from ativo.main import ativoClass, ativoOrcamentoClass
 from excipients.main import excipientClass
 from capsules.main import capsuleClass
 from embalagens.main import embalagemClass
+
 
 class orcamentoClass:
     ativos: list[ativoClass] = []
@@ -14,7 +16,7 @@ class orcamentoClass:
     possible_ativos: dict[str : list[ativoClass]] = {}
     number_of_capsule: int = 1
     compression_factor: float = 0.8
-    capsule: list[capsuleClass] = []
+    capsules: list[capsuleClass] = []
     excipiente = None
     embalagem = None
 
@@ -25,17 +27,27 @@ class orcamentoClass:
         forma_farmaceutica='',
         sub_forma_farmaceutica='',
     ) -> None:
-        self.ativos = ativos
-        self.quantity = quantity
+        self.ativos = []
+        for ativo in ativos:
+            ativo = ativoClass(
+                ativo['nome'],
+                ativoOrcamentoClass(int(ativo['quantidade']), ativo['unidade']),
+            )
+            self.ativos.append(ativo)
+        self.quantity = int(quantity)
         self.forma_farmaceutica = forma_farmaceutica
         self.sub_forma_farmaceutica = sub_forma_farmaceutica
 
-    def print_orcamento(self):
-        for ativo_name in self.possible_ativos.keys():
-            for ativo in self.possible_ativos[ativo_name]:
+    def print_orcamento(self, ativos):
+        if ativos:
+            for ativo in self.ativos:
                 print(ativo)
+        else:
+            for ativo_name in self.possible_ativos.keys():
+                for ativo in self.possible_ativos[ativo_name]:
+                    print(ativo)
         print(self.price_range)
-        for capsule in self.capsule:
+        for capsule in self.capsules:
             print(capsule)
         print(self.excipiente)
         print(self.embalagem)
@@ -51,14 +63,79 @@ class orcamentoClass:
     # TODO: implement this method to update the price without
     # choose anything, only using what it receives
     def getPrice(self, orcamento_values):
-        return
+        self.calc_price_ativos(True)
+        self.choose_capsule(orcamento_values['capsula']['tipo'])
+        self.excipiente = excipientClass()
+        self.excipiente.set_excipiente_values(orcamento_values['excipiente']['nome'])
+        self.excipiente.set_excipiente_orcamento(
+            self.calc_quantity_excipiente(), orcamento_values['excipiente']['unidade']
+        )
+        self.calc_price(self.excipiente.ativo)
+        self.embalagem = embalagemClass()
+        self.embalagem.set_embalagem_values(
+            None,
+            orcamento_values['embalagem']['nome'],
+        )
+
+        self.embalagem.calc_price(orcamento_values['embalagem']['quantidade'])
+        orcamento = {
+            'nomeCliente': 'Maria',
+            'quantidade': 60,
+            'nomeMedico': 'João',
+            'formaFarmaceutica': orcamento_values['formaFarmaceutica'],
+            'formaFarmaceuticaSubgrupo': orcamento_values['formaFarmaceuticaSubgrupo'],
+            'ativos': [],
+            'embalagem': {
+                'nome': self.embalagem.ativo.name,
+                'unidade': self.embalagem.ativo.orcamento.unity,
+                'quantidade': self.embalagem.ativo.orcamento.quantity,
+                'preco': self.embalagem.ativo.orcamento.price,
+            },
+            'excipiente': {
+                'nome': self.excipiente.ativo.name,
+                'unidade': self.excipiente.ativo.orcamento.unity,
+                'quantidade': self.excipiente.ativo.orcamento.quantity,
+                'preco': self.excipiente.ativo.orcamento.price,
+            },
+            'capsulas': [],
+            'custoFixo': 7.80,
+        }
+        for ativo in self.ativos:
+            orcamento['ativos'].append(
+                {
+                    'unidade': ativo.orcamento.unity,
+                    'quantidade': ativo.orcamento.quantity,
+                    'opcoes': [
+                        {
+                            'nome': ativo.name,
+                            'preco': ativo.orcamento.price,
+                        }
+                    ],
+                }
+            )
+        for capsule in self.capsules:
+            orcamento['capsulas'].append(
+                {
+                    'tipo': capsule.type,
+                    'nome': capsule.ativo.name,
+                    'quantidade': self.quantity,
+                    'contem': self.number_of_capsule,
+                    'preco': capsule.ativo.orcamento.price,
+                }
+            )
+
+        return orcamento
 
     def do_descr_match(self, target, df):
         if df['DESCR'].str.contains(target, case=False, na=False).any():
             return df[df['DESCR'].str.contains(target, case=False, na=False)]
         return []
 
-    def find_closest_match_contains(self, df, target):
+    def find_closest_match_contains(self, df, target, exact):
+        # Exact match
+        if exact:
+            return df[df['DESCR'] == target]
+
         # Step 1: Full match
         matchs = self.do_descr_match(target, df)
         if len(matchs) > 0:
@@ -82,37 +159,19 @@ class orcamentoClass:
         return None  # No match found
 
     def parse_ativo_fields(self, row_ativo, ativoOrcado: ativoClass):
-        ativo = ativoClass(
-            row_ativo['DESCR'],
-            ativoOrcamentoClass(
-                ativoOrcado.orcamento.quantity,
-                ativoOrcado.orcamento.unity,
-            ),
+        ativoOrcado.name = row_ativo['DESCR']
+        ativoOrcado.orcamento = ativoOrcamentoClass(
+            ativoOrcado.orcamento.quantity,
+            ativoOrcado.orcamento.unity,
         )
-        ativo.price = row_ativo['PRVEN']
-        ativo.equivalency = row_ativo['EQUIV']
-        ativo.dilution = row_ativo['DILUICAO']
-        ativo.density = row_ativo['DENSIDADE']
-        if isinstance(row_ativo['ARGUMENTO'], str):
-            ativo.unity_conversion = row_ativo['ARGUMENTO']
-            ativo.unity_value_conversion = float(row_ativo['PARAMETRO'])
-        
-        return ativo
 
-    def unityCalcConversion(self, unity: str):
-        unity = unity.upper()
-        if unity == '%':
-            return 0.01
-        elif unity == 'MG':
-            return 0.001
-        elif unity == 'MCG':
-            return 0.000001
-        elif unity == 'KG':
-            return 1000
-        elif unity == 'L':
-            return 1000
-        else:
-            return 1
+        ativoOrcado.price = row_ativo['PRVEN']
+        ativoOrcado.equivalency = row_ativo['EQUIV']
+        ativoOrcado.dilution = row_ativo['DILUICAO']
+        ativoOrcado.density = row_ativo['DENSIDADE']
+        if isinstance(row_ativo['ARGUMENTO'], str):
+            ativoOrcado.unity_conversion = row_ativo['ARGUMENTO']
+            ativoOrcado.unity_value_conversion = float(row_ativo['PARAMETRO'])
 
     def calc_price(self, ativo: ativoClass):
         ativo.orcamento.price = (
@@ -120,26 +179,28 @@ class orcamentoClass:
             * ativo.dilution
             * ativo.equivalency
             * ativo.orcamento.quantity
-            * self.unityCalcConversion(ativo.orcamento.unity)
+            * utils.unityCalcConversion(ativo.orcamento.unity)
             / ativo.unity_value_conversion
         )
         if self.forma_farmaceutica not in ['']:
             ativo.orcamento.price *= self.quantity
 
-    def calc_price_ativos(self):
+    def calc_price_ativos(self, exact=False):
         df_ativos = pd.read_csv(
             './orcamento_tables/smart/ativos_joined_FCerta_SMART_2024.csv'
         )
-        
+
         for ativoOrcado in self.ativos:
-            df_match = self.find_closest_match_contains(df_ativos, ativoOrcado.name)
+            df_match = self.find_closest_match_contains(
+                df_ativos, ativoOrcado.name, exact
+            )
             self.possible_ativos[ativoOrcado.name] = []
             for row in df_match.iterrows():
                 row = row[1].to_dict()
-                ativo = self.parse_ativo_fields(row, ativoOrcado)
-                self.calc_price(ativo)
-                self.possible_ativos[ativoOrcado.name].append(ativo)
-    
+                self.parse_ativo_fields(row, ativoOrcado)
+                self.calc_price(ativoOrcado)
+                self.possible_ativos[ativoOrcado.name].append(ativoOrcado)
+
     def calc_price_range(self):
         for ativo_name in self.possible_ativos:
             prices = []
@@ -152,7 +213,7 @@ class orcamentoClass:
     def calc_volume_ativos(self):
         ativos_volume = 0
         for ativo in self.ativos:
-            ativos_volume += ativo.orcamento.quantity / (
+            ativos_volume += (ativo.orcamento.quantity * ativo.dilution) / (
                 ativo.unity_value_conversion * ativo.density
             )
 
@@ -167,35 +228,43 @@ class orcamentoClass:
         )
         number_of_capsule = 0
         ativo_volume = self.calc_volume_ativos()
-        while len(self.capsule) == 0:
+        while len(self.capsules) == 0:
             number_of_capsule += 1
             for row in capsules.iterrows():
                 row = row[1].to_dict()
                 if ativo_volume <= (row['VOLINTERNO'] * number_of_capsule):
-                    self.capsule.append(
-                        capsuleClass(
-                            row['DESCRICAO'],
-                            row['DESC_COR'],
+                    capsule = capsuleClass(
+                        row['DESCRICAO'],
+                        row['DESC_COR'],
+                        row['VOLINTERNO'],
+                        row['VOLEXTERNO'],
+                        row['PRIORIDADE'],
+                        ativoClass(
                             row['DESCR'],
-                            row['VOLINTERNO'],
-                            row['VOLEXTERNO'],
-                        )
+                            ativoOrcamentoClass(
+                                number_of_capsule,
+                                'UN',
+                            ),
+                        ),
                     )
                     self.number_of_capsule = number_of_capsule
+                    self.calc_price(capsule.ativo)
+                    self.capsules.append(capsule)
+                    break
 
     def choose_excipiente(self):
         self.excipiente = excipientClass()
-        self.excipiente.quantity = self.calc_quantity_excipiente()
+        self.excipiente.ativo.orcamento.quantity = self.calc_quantity_excipiente()
         self.excipiente.set_excipiente_name(self.sub_forma_farmaceutica, self.ativos)
 
     def calc_quantity_excipiente(self):
         ativos_volume = self.calc_volume_ativos()
-        return ativos_volume - self.capsule[0].internal_volume
-    
+        return (self.number_of_capsule * self.capsules[0].internal_volume) - ativos_volume
+
     def calc_volume_capsule(self):
-        return self.capsule[0].external_volume * self.quantity
-        
+        return self.capsules[0].external_volume * self.quantity
+
     def choose_embalagem(self):
         self.embalagem = embalagemClass()
         volume_capsule = self.calc_volume_capsule()
-        self.embalagem.getEmbalagem(self.quantity)
+        self.embalagem.set_embalagem_values(self.quantity)
