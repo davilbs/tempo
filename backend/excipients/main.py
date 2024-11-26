6549d34ac9ede7d9ac6d4c102737867b1a7fde93
Yuri Pereira <yuri@anaai.com.br>
2024-11-26 16:00:46 -0300

    Adequating excipientes to set values to the capsule to be as an ativoClass

--- backend/excipients/main.py
+++ backend/excipients/main.py
@@ -1,15 +1,30 @@
-from excipients.rules import excipientRules
+import pandas as pd
 
+from excipients.rules import excipientRules
+from ativo.main import ativoClass, ativoOrcamentoClass
 
 class excipientClass:
-    name = ''
-    quantity = 0
-    unity = 'MG'
+    ativo: ativoClass = None
     rule = None
     
     def set_excipiente_name(self, sub_forma_farmaceutica, ativos):
         self.rule = excipientRules(sub_forma_farmaceutica)
-        self.name = self.rule.get_excipiente(ativos)
+        self.ativo = ativoClass(self.rule.get_excipiente(ativos), None)
+        self.set_excipiente_values(self.ativo.name)
+
+    def set_excipiente_values(self, name):
+        df_excipientes = pd.read_csv(
+            './orcamento_tables/smart/excipientes_FCerta_SMART_2024.csv'
+        )
+        embalagem = df_excipientes[df_excipientes['DESCR'] == name]
+        self.ativo = ativoClass(name, None)
+        self.ativo.density = embalagem['DENSIDADE'].iloc[0]
+        self.ativo.dilution = embalagem['DILUICAO'].iloc[0]
+        self.ativo.equivalency = embalagem['EQUIV'].iloc[0]
+        self.ativo.price = embalagem['PRVEN'].iloc[0]
+        
+    def set_excipiente_orcamento(self, quantity, unity):
+        self.ativo.orcamento = ativoOrcamentoClass(quantity, unity)
 
     def __str__(self):
-        return f"name: {self.name}, quantity: {self.quantity}, unity: {self.unity}"
+        return f"name: {self.ativo.name}, quantity: {self.ativo.orcamento.quantity}, unity: {self.ativo.orcamento.unity}"
