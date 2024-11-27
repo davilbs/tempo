caca0e10190c9b6a77753c6b51b30b006105b803
Yuri Pereira <yuri@anaai.com.br>
2024-11-27 14:01:30 -0300

    Fixing initialize of excipiente and set values

--- backend/excipients/main.py
+++ backend/excipients/main.py
@@ -3,18 +3,19 @@ import pandas as pd
 from excipients.rules import excipientRules
 from ativo.main import ativoClass
 
-class excipientClass(ativoClass):    
-    def set_excipiente_name(self, sub_forma_farmaceutica, ativos):
+class excipientClass(ativoClass):
+    def __init__(self, sub_forma_farmaceutica, ativos) -> None:
         rule = excipientRules(sub_forma_farmaceutica)
-        self.ativo = ativoClass(rule.get_excipiente(ativos))
-        self.set_values(self.ativo.name)
+        name = rule.get_excipiente(ativos)
+        super().__init__(name)
+        self.set_values()
 
     def set_values(self):
         df_excipientes = pd.read_csv(
             './orcamento_tables/smart/excipientes_FCerta_SMART_2024.csv'
         )
-        embalagem = df_excipientes[df_excipientes['DESCR'] == self.ativo.name]
-        self.ativo.density = embalagem['DENSIDADE'].iloc[0]
-        self.ativo.dilution = embalagem['DILUICAO'].iloc[0]
-        self.ativo.equivalency = embalagem['EQUIV'].iloc[0]
-        self.ativo.price = embalagem['PRVEN'].iloc[0]
+        excipiente = df_excipientes[df_excipientes['DESCR'] == self.name]
+        self.density = excipiente['DENSIDADE'].iloc[0]
+        self.dilution = excipiente['DILUICAO'].iloc[0]
+        self.equivalency = excipiente['EQUIV'].iloc[0]
+        self.price = excipiente['PRVEN'].iloc[0]
