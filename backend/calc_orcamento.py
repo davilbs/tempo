d0b5282ac1693266999723396bc1318e496dcf8b
Yuri Pereira <yuri@anaai.com.br>
2024-11-27 18:14:13 -0300

    Adjusting calc_orcamento to mock both types of orcamento

--- backend/calc_orcamento.py
+++ backend/calc_orcamento.py
@@ -1,4 +1,5 @@
 from pre_orcamento import preOrcamentoClass
+from orcamento import orcamentoClass
 
 
 if __name__ == '__main__':
@@ -24,34 +25,35 @@ if __name__ == '__main__':
         nome_cliente='Maria',
     )
     orcamento.create_pre_orcamento()
-    # orcamento_body = {
-    #     'quantity': 60,
-    #     'formaFarmaceutica': '1 - Cápsula',
-    #     'formaFarmaceuticaSubgrupo': 'Slow Release',
-    #     'ativos': [
-    #         {
-    #             'nome': 'FENUGREEK (50% FENUSIDEOS)',
-    #             'unidade': 'MG',
-    #             'quantidade': 200,
-    #         },
-    #         {'nome': 'MACA', 'unidade': 'MG', 'quantidade': 500},
-    #         {
-    #             'nome': 'GINKGO BILOBA EXTRACT 2:1',
-    #             'unidade': 'MG',
-    #             'quantidade': 100,
-    #         },
-    #     ],
-    #     'embalagem': {'nome': 'POTE CAPS 310ML', 'quantidade': 1},
-    #     'excipiente': {
-    #         'nome': 'EXCIPIENTE PADRÃO CÁPSULAS',
-    #         'unidade': 'MG',
-    #         'quantidade': 173,
-    #     },
-    #     'capsula': {'tipo': 'INCOLOR', 'unidade': 'UN', 'quantidade': 60},
-    # }
-    # orcamentoClass(
-    #     orcamento_body['ativos'],
-    #     orcamento_body['quantity'],
-    #     forma_farmaceutica='1 - Cápsula',
-    #     sub_forma_farmaceutica='Slow Release',
-    # ).getPrice(orcamento_body)
+    orcamento_body = {
+        'nome_cliente': 'Maria',
+        'nome_medico': 'João',
+        'dosagem': 60,
+        'forma_farmaceutica': '1 - Cápsula',
+        'sub_forma_farmaceutica': 'Slow Release',
+        'ativos': [
+            {
+                'nome': 'FENUGREEK (50% FENUSIDEOS)',
+                'unidade': 'MG',
+                'quantidade': 200,
+            },
+            {'nome': 'MACA', 'unidade': 'MG', 'quantidade': 500},
+            {
+                'nome': 'GINKGO BILOBA EXTRACT 2:1',
+                'unidade': 'MG',
+                'quantidade': 100,
+            },
+        ],
+        'embalagem': {'nome': 'POTE CAPS 310ML', 'quantidade': 1},
+        'excipiente': {
+            'nome': 'EXCIPIENTE PADRÃO CÁPSULAS',
+            'unidade': 'MG',
+            'quantidade': '-',
+        },
+        'capsula': {'tipo': 'INCOLOR', 'unidade': 'UN', 'quantidade': 60},
+    }
+    orcamento = orcamentoClass(
+        orcamento_body,
+    )
+    orcamento.create_orcamento()
+    
