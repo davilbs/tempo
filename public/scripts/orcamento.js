92a4fb62885a172b7656dbda1200c588535de895
Yuri Pereira <yuri@anaai.com.br>
2024-12-05 14:55:20 -0300

    Removing unused functions. Changing quantitys to be as float. Changing to treat orcamentos as list

--- public/scripts/orcamento.js
+++ public/scripts/orcamento.js
@@ -1,62 +1,33 @@
+const ativosAll = JSON.parse(sessionStorage.getItem('ativos'));
+
 window.onload = () => {
     updateTotal();
 };
 
-function handleAtivoChange(rowIndex) {
-    // Get the dropdown for the current row
-    const selectElement = document.getElementById(`ativoPuro-${rowIndex}`);
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-
-    // Extract the price from the selected option
-    const preco = selectedOption.getAttribute('data-preco');
-
-    // Update the corresponding Preço cell
-    document.getElementById(`preco-ativo-${rowIndex}`).innerText =
-        preco !== '-' ? `R$${parseFloat(preco).toFixed(2)}` : preco;
-
-    updateTotal();
-}
-
 function updateTotal() {
-    let total = 0;
+    const tables = document.querySelectorAll("div[id^='orcamento-']")
+    tables.forEach((table) => {
+        let total = 0;
+        // Loop through all rows to calculate the sum of selected prices
+        const rows = table.querySelectorAll("table tbody tr");
+        rows.forEach((row, index) => {
+            var tds = row.querySelectorAll('td');
 
-    // Loop through all rows to calculate the sum of selected prices
-    const rows = document.querySelectorAll("table tbody tr");
-    rows.forEach((row, index) => {
-        var tds = row.querySelectorAll('td');
+            if (tds[tds.length - 1].id.includes('preco-')) {
+                const precoText = tds[tds.length - 1].innerText;
 
-        if (tds[tds.length - 1].id.includes('preco-')) {
-            const precoText = tds[tds.length - 1].innerText;
-
-            if (precoText && precoText.startsWith("R$")) {
-                const preco = parseFloat(precoText.replace("R$", "").replace(",", "."));
-                if (!isNaN(preco)) {
-                    total += preco;
+                if (precoText && precoText.startsWith("R$")) {
+                    const preco = parseFloat(precoText.replace("R$", "").replace(",", "."));
+                    if (!isNaN(preco)) {
+                        total += preco;
+                    }
                 }
             }
-        }
-    });
+        });
 
-    // Update the Total cell
-    const totalCell = document.getElementById("total-preco");
-    totalCell.innerText = `R$${total.toFixed(2)}`;
-}
-
-function updateSubgrupoDropdown() {
-    const formaFarmaceutica = document.getElementById('forma-farmaceutica').value;
-    const subgrupoDropdown = document.getElementById('forma-farmaceutica-subgrupo');
-
-    subgrupoDropdown.innerHTML = '';
-
-    const subgrupoOptions = formaFarmaceuticaSubgrupoAll[formaFarmaceutica];
-    subgrupoOptions.forEach((subgrupo, index) => {
-        const option = document.createElement('option');
-        option.value = subgrupo;
-        option.textContent = subgrupo;
-        if (index === 0) {
-            option.selected = true;
-        }
-        subgrupoDropdown.appendChild(option);
+        // Update the Total cell
+        const totalCell = table.querySelector("td[id='total-preco']");
+        totalCell.innerText = `R$${total.toFixed(2)}`;
     });
 }
 
@@ -136,130 +107,137 @@ function updateCapsulaPrice() {
 }
 
 function parse_orcamento() {
-    var ativos = [];
-    var embalagem = {};
-    var excipiente = {};
-    var capsula = {}
-    const rows = document.querySelectorAll("table tbody tr");
-    var ativoCounter = 0;
-    const ativosAll = JSON.parse(ativosList);
-    rows.forEach((tr) => {
-        var ativo = {};
-        var tds = tr.querySelectorAll('td');
-        console.log("Loading tr", tr.id);
-        if (tr.id == 'ativo') {
-            tds.forEach((td) => {
-                if (td.querySelector('select')) {
-                    td = td.querySelector('select');
-                    const values = Array.from(td.options).map(option => option.value);
-                    ativo['opcoes'] = [];
-                    for (let i = 0; i < values.length; i++) {
-                        var element = values[i];
-                        var price = ativosAll[ativoCounter].opcoes[i]['preco'];
-                        ativo['opcoes'].push(
+    var orcamentos = []
+    var orcamentoCounter = 0;
+    const tables = document.querySelectorAll("div[id^='orcamento-']")
+    tables.forEach((table) => {
+        var ativos = [];
+        var embalagem = {};
+        var excipiente = {};
+        var capsula = {}
+        var ativoCounter = 0;
+        const rows = table.querySelectorAll("table tbody tr");
+        rows.forEach((tr) => {
+            var ativo = {};
+            var tds = tr.querySelectorAll('td');
+            if (tr.id == 'ativo') {
+                tds.forEach((td) => {
+                    if (td.id.includes('ativo-puro-')) {
+                        ativo['opcoes'] = [
                             {
-                                'nome': element,
-                                'preco': price
+                                'nome': td.innerText,
+                                'preco': 0.0
                             }
-                        );
+                        ];
+                        for (let i = 0; i < ativosAll[orcamentoCounter][ativoCounter].length; i++) {
+                            var element = ativosAll[orcamentoCounter][ativoCounter][i];
+                            if (element.nome != ''){
+                                ativo['opcoes'].push(element);
+                            }
+                        }
+                        ativoCounter += 1;
                     }
-                }
-                else if (td.id.includes('ativo-unidade')) {
-                    ativo['unidade'] = td.innerText;
-                }
-                else if (td.id.includes('ativo-quantidade')) {
-                    ativo['quantidade'] = parseInt(td.innerText);
-                }
-            });
-            ativos = ativos.concat(ativo);
-            ativoCounter += 1;
-        }
-        else if (tr.id == 'embalagem') {
-            tds.forEach((td) => {
-                if (td.querySelector('select')) {
-                    td = td.querySelector('select');
-                }
-                else if (td.querySelector('input')) {
-                    td = td.querySelector('input');
-                }
-                console.log("EMBALAGEM", td);
-                if (td.id.includes('embalagem-nome')) {
-                    embalagem['nome'] = td.innerText;
-                }
-                else if (td.id.includes('embalagem-unidade')) {
-                    embalagem['unidade'] = td.innerText;
-                }
-                else if (td.id.includes('embalagem-quantidade')) {
-                    embalagem['quantidade'] = parseInt(td.innerText);
-                }
-                else if (td.id.includes('preco-embalagem')) {
-                    embalagem['preco'] = parseFloat(td.innerText.replace("R$", ""));
-                }
-            });
-        }
-        else if (tr.id == 'excipiente') {
-            tds.forEach((td) => {
-                if (td.querySelector('select')) {
-                    td = td.querySelector('select');
-                }
-                else if (td.querySelector('input')) {
-                    td = td.querySelector('input');
-                }
-                if (td.id.includes('excipiente-nome')) {
-                    excipiente['nome'] = td.innerText;
-                }
-                else if (td.id.includes('excipiente-unidade')) {
-                    excipiente['unidade'] = td.innerText;
-                }
-                else if (td.id.includes('excipiente-quantidade')) {
-                    excipiente['quantidade'] = parseInt(td.innerText);
-                }
-                else if (td.id.includes('preco-excipiente')) {
-                    excipiente['preco'] = parseFloat(td.innerText.replace("R$", ""));
-                }
-            });
-        }
-        else if (tr.id == 'capsula') {
-            tds.forEach((td) => {
-                if (td.querySelector('select')) {
-                    td = td.querySelector('select');
-                }
-                else if (td.querySelector('input')) {
-                    td = td.querySelector('input');
-                }
-                if (td.id.includes('capsula-tipo')) {
-                    capsula['tipo'] = td.innerText;
-                }
-                else if (td.id.includes('capsula-nome')) {
-                    capsula['nome'] = td.innerText;
-                }
-                else if (td.id.includes('capsula-contem')) {
-                    capsula['contem'] = td.innerText;
-                }
-                else if (td.id.includes('capsula-unidade')) {
-                    capsula['unidade'] = td.innerText;
-                }
-                else if (td.id.includes('capsula-quantidade')) {
-                    capsula['quantidade'] = parseInt(td.innerText);
-                }
-                else if (td.id.includes('preco-capsula')) {
-                    capsula['preco'] = parseFloat(td.innerText.replace("R$", ""));
-                }
-            });
-        }
+                    else if (td.id.includes('ativo-unidade')) {
+                        ativo['unidade'] = td.innerText;
+                    }
+                    else if (td.id.includes('ativo-quantidade')) {
+                        ativo['quantidade'] = parseFloat(td.innerText);
+                    }
+                });
+                ativo['opcoes'] = ativo['opcoes'].filter((item, index, self) =>
+                    index === self.findIndex((t) => t.nome === item.nome && t.preço === item.preço)
+                );
+                ativos = ativos.concat(ativo);
+            }
+            else if (tr.id == 'embalagem') {
+                tds.forEach((td) => {
+                    if (td.querySelector('select')) {
+                        td = td.querySelector('select');
+                    }
+                    else if (td.querySelector('input')) {
+                        td = td.querySelector('input');
+                    }
+                    if (td.id.includes('embalagem-nome')) {
+                        embalagem['nome'] = td.innerText;
+                    }
+                    else if (td.id.includes('embalagem-unidade')) {
+                        embalagem['unidade'] = td.innerText;
+                    }
+                    else if (td.id.includes('embalagem-quantidade')) {
+                        embalagem['quantidade'] = parseFloat(td.innerText);
+                    }
+                    else if (td.id.includes('preco-embalagem')) {
+                        embalagem['preco'] = parseFloat(td.innerText.replace("R$", ""));
+                    }
+                });
+            }
+            else if (tr.id == 'excipiente') {
+                tds.forEach((td) => {
+                    if (td.querySelector('select')) {
+                        td = td.querySelector('select');
+                    }
+                    else if (td.querySelector('input')) {
+                        td = td.querySelector('input');
+                    }
+                    if (td.id.includes('excipiente-nome')) {
+                        excipiente['nome'] = td.innerText;
+                    }
+                    else if (td.id.includes('excipiente-unidade')) {
+                        excipiente['unidade'] = td.innerText;
+                    }
+                    else if (td.id.includes('excipiente-quantidade')) {
+                        excipiente['quantidade'] = parseFloat(td.innerText);
+                    }
+                    else if (td.id.includes('preco-excipiente')) {
+                        excipiente['preco'] = parseFloat(td.innerText.replace("R$", ""));
+                    }
+                });
+            }
+            else if (tr.id == 'capsula') {
+                tds.forEach((td) => {
+                    if (td.querySelector('select')) {
+                        td = td.querySelector('select');
+                    }
+                    else if (td.querySelector('input')) {
+                        td = td.querySelector('input');
+                    }
+                    if (td.id.includes('capsula-tipo')) {
+                        capsula['tipo'] = td.innerText;
+                    }
+                    else if (td.id.includes('capsula-nome')) {
+                        capsula['nome'] = td.innerText;
+                    }
+                    else if (td.id.includes('capsula-contem')) {
+                        capsula['contem'] = td.innerText;
+                    }
+                    else if (td.id.includes('capsula-unidade')) {
+                        capsula['unidade'] = td.innerText;
+                    }
+                    else if (td.id.includes('capsula-quantidade')) {
+                        capsula['quantidade'] = parseFloat(td.innerText);
+                    }
+                    else if (td.id.includes('preco-capsula')) {
+                        capsula['preco'] = parseFloat(td.innerText.replace("R$", ""));
+                    }
+                });
+            }
+        });
+        orcamentos.push({
+            "nomeCliente": document.getElementById('nome-cliente').innerText,
+            "nomeMedico": document.getElementById('nome-medico').innerText,
+            "dosagem": parseFloat(document.getElementById('quantidade-orcamento').innerText),
+            "custoFixo": parseFloat(document.getElementById('preco-custo-fixo').innerText.replace("R$", "")),
+            "formaFarmaceutica": document.getElementById('forma-farmaceutica').innerText,
+            "formaFarmaceuticaSubgrupo": document.getElementById('forma-farmaceutica-subgrupo').innerText,
+            "ativos": ativos,
+            "embalagem": embalagem,
+            "excipiente": excipiente,
+            "capsula": capsula,
+        });
+        orcamentoCounter += 1;
     });
-    return {
-        "nomeCliente": document.getElementById('nome-cliente').innerText,
-        "nomeMedico": document.getElementById('nome-medico').innerText,
-        "dosagem": parseInt(document.getElementById('quantidade-orcamento').innerText),
-        "custoFixo": parseFloat(document.getElementById('preco-custo-fixo').innerText.replace("R$", "")),
-        "formaFarmaceutica": document.getElementById('forma-farmaceutica').innerText,
-        "formaFarmaceuticaSubgrupo": document.getElementById('forma-farmaceutica-subgrupo').innerText,
-        "ativos": ativos,
-        "embalagem": embalagem,
-        "excipiente": excipiente,
-        "capsula": capsula,
-    };
+
+    return orcamentos;
 }
 
 document.getElementById('edit_orcamento').addEventListener('click', async () => {
