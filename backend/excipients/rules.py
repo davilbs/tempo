75236fe415f84d7aacba7519d281a27dbd7e4c12
Yuri Pereira <yuri@anaai.com.br>
2024-12-17 10:38:43 -0300

    striping multiple spaces and change how to get the excipientes/rules

--- backend/excipients/rules.py
+++ backend/excipients/rules.py
@@ -13,28 +13,43 @@ class excipientRules:
         return self.check_sub_forma_farmaceutica(ativos)
 
     def check_sub_forma_farmaceutica(self, ativos):
+        excipiente = ''
         if self.sub_forma_farmaceutica in [
             "Slow Release",
             "Hipoalergênica",
             "Sublingual",
+            "Iodo Metalóide + Iodeto de Potássio",
+            "Lipofílica / Oleosa",
+            "Thyroid ou Tireodei em Grão",
+            "Óleo ozonizado",
         ]:
             if self.sub_forma_farmaceutica == 'Slow Release':
-                return 'EXCIPIENTE SLOW RELEASE II'
+                excipiente = 'EXCIPIENTE SLOW RELEASE II'
             elif self.sub_forma_farmaceutica == 'Hipoalergênica':
-                return 'EXCIP HIPOALERGENICO CAPS-VEG'
+                excipiente = 'EXCIP HIPOALERGENICO CAPS-VEG'
             elif self.sub_forma_farmaceutica == 'Sublingual':
-                return 'EXCIPIENTE CAPSULA SUBLINGUAL'
+                excipiente = 'EXCIPIENTE CAPSULA SUBLINGUAL'
             elif (
                 self.sub_forma_farmaceutica == 'Iodo Metalóide + Iodeto de Potássio'
             ):
-                return self.iodo(ativos)
+                excipiente = self.iodo(ativos)
             elif self.sub_forma_farmaceutica == "Lipofílica / Oleosa":
-                return self.lipofilico_oleosa(ativos)
-        return 'EXCIPIENTE PADRÃO CÁPSULAS'
+                excipiente = self.lipofilico_oleosa(ativos)
+            elif self.sub_forma_farmaceutica == "Thyroid ou Tireodei em Grão":
+                excipiente = self.thyroid(ativos)
+            elif self.sub_forma_farmaceutica == "Óleo ozonizado":
+                excipiente = self.oleo_ozonizado(ativos)
+            elif self.sub_forma_farmaceutica == "Vaginal":
+                excipiente = self.uso_vaginal(ativos)
+        
+        if excipiente == '' or excipiente == None:
+            excipiente = 'EXCIPIENTE PADRÃO CÁPSULAS'
+        
+        return excipiente
 
     def lipofilico_oleosa(self, ativos: list[ativoClass]):
         for ativo in ativos:
-            if re.match('lipofilic', ativo.name):
+            if re.match(r'lipofilic', ativo.name):
                 return 'EXCIPIENTE LIPOFILICO'
 
         return ''
@@ -44,10 +59,10 @@ class excipientRules:
 
         for ativo in ativos:
             ativo_name = unidecode(ativo.name).lower()
-            if re.match('iodo metaloide', ativo_name):
+            if re.match(r'iodo metaloide', ativo_name):
                 ativo_iodo = ativo
                 match += 1
-            elif re.match('iodeto potassio', ativo_name):
+            elif re.match(r'iodeto potassio', ativo_name):
                 ativo_iodeto = ativo
                 match += 1
             if match == 2:
@@ -59,6 +74,26 @@ class excipientRules:
                     ),
                 )
                 ativo_iodeto = ativoClass('', None)
-                return 'EXCIPIENTE LIPOFILICO'
+                return 'EXCIPIENTE IODORAL'
 
-        return ''
+    def thyroid(self, ativos: list[ativoClass]):
+        for ativo in ativos:
+            ativo_unity = unidecode(ativo.orcamento.unity).lower()
+            if re.match(r'grao', ativo_unity):
+                ativo.orcamento.quantity = ativo.orcamento.quantity * 65
+                ativo.orcamento.unity = 'MG'
+    
+    def oleo_ozonizado(self,  ativos: list[ativoClass]):
+        for ativo in ativos:
+            ativo_name = unidecode(ativo.name).lower()
+            if re.match(r'ol.+ozonizado.+p\/caps', ativo_name):
+                ativo.orcamento.quantity = ativo.orcamento.quantity * 10
+                ativo.orcamento.unity = 'MG'
+    
+
+    def uso_vaginal(self,  ativos: list[ativoClass]):
+        for ativo in ativos:
+            ativo_name = unidecode(ativo.name).lower()
+            if re.match('uso vaginal', ativo_name):
+                ativo.orcamento.quantity = ativo.orcamento.quantity * 10
+                ativo.orcamento.unity = 'MG'
